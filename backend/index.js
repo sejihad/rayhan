6129b6944cb63be458ed47598818cb6b6633f4c8
SE JIHAD <ijihad639@gmail.com>
2026-06-10 19:27:36 +0600

    fix

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://127.0.0.1:5173",
   "http://rayhanislamrokon.com",
+  "https://rayhanislamrokon.com",
 ].filter(Boolean);
 
 app.use(
